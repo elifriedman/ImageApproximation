47ea0bdf28386f93fb284d9c611181aa463225d1
Eli Friedman <friedm3@cooper.edu>
2019-01-04 16:10:53 +0200

    update shapes

--- shapes.py
+++ shapes.py
@@ -1,20 +1,21 @@
 from math import sqrt
 import numpy as np
+from PIL import Image, ImageDraw
 
 class DrawUtils:
 
     @staticmethod
     def color(image_segment, color, alpha):
-        image_segment[:] = alpha*color + (1.0 - alpha) * image_segment
+        image_segment[:] = alpha*np.array(color) + (1.0 - alpha) * image_segment
+
 
 class Shape(object):
     """A shape object
     """
-
     def __init__(self, pos, color, alpha):
         self.x = pos[0]
         self.y = pos[1]
-        self.color = np.asarray(color)
+        self.color = color
         self.alpha = alpha
         self._params = [*pos, *color, alpha]
 
@@ -31,16 +32,55 @@ class Shape(object):
         return self._params
 
     @classmethod
-    def random_params(cls, shape):
-        x = np.random.randint(0, shape[0])
-        y = np.random.randint(0, shape[1])
-        pos = (x, y)
+    def random_params(cls):
+        pos = np.random.rand(2)
         color = np.random.rand(3)
-        alpha = np.random.rand()
-        return [x, y, *color, alpha]
+        alpha = np.random.rand()*.99  # don't want anything opaque
+        return [*pos, *color, alpha]
+
+    def denorm(self, image):
+        try:
+            h, w, _ = image.shape
+        except AttributeError:
+            w, h = image.size
+        x = int(self.x * h)
+        y = int(self.y * w)
+        return [(x, y), self.color, self.alpha]
 
     def draw(self, image):
-        pass
+        self.denorm(image)
+
+
+class Rectangle(Shape):
+    def __init__(self, pos, color, alpha, width, height):
+        super().__init__(pos, color, alpha)
+        self.width = width
+        self.height = height
+        self.params.extend([self.width, self.height])
+
+    @classmethod
+    def random_params(cls):
+        params = super().random_params()
+        width, height = np.random.rand(2)
+        params.extend([width, height])
+        return params
+
+    def denorm(self, image):
+        h, w = image.size
+        height = int(h*self.height)
+        width = int(w*self.width)
+
+        params = super().denorm(image)
+        params.extend([width, height])
+        return params
+
+    def draw(self, image):
+        (x, y), c, a, w, h = self.denorm(image)
+
+        bbox = [(x, y), (x+h, y+w)]
+        drw = ImageDraw.Draw(image, 'RGBA')
+        drw.rectangle(bbox, tuple([int(c*255) for c in self.color] + [int(self.alpha*255)]))
+
 
 class Circle(Shape):
     def __init__(self, pos, color, alpha, radius):
@@ -49,27 +89,24 @@ class Circle(Shape):
         self._params.append(self.radius)
 
     @classmethod
-    def random_params(cls, shape):
-        params = super().random_params(shape)
-        radius = np.random.randint(max(shape))
+    def random_params(cls):
+        params = super().random_params()
+        radius = 0.4*np.random.rand()
         params.append(radius)
         return params
 
+    def denorm(self, image):
+        params = super().denorm(image)
+        params.append(int(self.radius*min(image.size)))
+        return params
+
     def draw(self, image):
-        rx = self.radius
-        rx2 = rx**2
-        for i in range(0, self.radius):
-            topx = min(image.shape[0]-1, max(self.x + i, 0))
-            botx = min(image.shape[0]-1, max(self.x - i, 0))
-
-            left  = min(image.shape[1]-1, max(-rx + self.y, 0))
-            right = min(image.shape[1]-1, max(rx + self.y, 0))
-            DrawUtils.color(image[topx, left:right], self.color, self.alpha)
-            if i > 0:
-                DrawUtils.color(image[botx, left:right], self.color, self.alpha)
-            rx2 = rx2 - 2*i - 1
-            rx = int(sqrt(rx2 if rx2 >= 0 else 0))
-
+        (x, y), color, alpha, radius = self.denorm(image)
+        
+        topleft = (x - radius, y - radius)
+        botright =  (x + radius, y + radius)
+        drw = ImageDraw.Draw(image, 'RGBA')
+        drw.ellipse((topleft, botright), tuple([int(c*255) for c in self.color] + [int(self.alpha*255)]))
 
 
 def drawcircs(image, N):
@@ -77,12 +114,7 @@ def drawcircs(image, N):
     w = 600
     circles = []
     for i in range(N):
-        x = np.random.randint(0, h)
-        y = np.random.randint(0, w)
-        color = np.random.rand(3)
-        alpha = np.random.rand()
-        radius = np.random.randint(0, max(h, w)//2)
-        param = [x, y, color, alpha, radius]
+        param = Circle.random_params((h, w))
         c = Circle.init_from_params(param)
         circles.append(c)
         c.draw(image)
